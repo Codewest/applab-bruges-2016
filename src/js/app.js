058f660caba0a400038f8ae344220909fcdb20c1
Sam <sam.demeulenaere@student.howest.be>
2016-10-14 16:39:33 +0200

    added route

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 (function() {
     $(function () {
         makeMap();
+        $("#route").on('click', openRoute);
     });
 
     var makeMap = function makeMap() {
@@ -12,4 +13,15 @@
             zoom: 13.7
         });
     };
+
+    var openRoute = function openRoute() {
+        var xStart = 51.2092336; //user x-position here (GEOLOCATION)
+        var yStart = 3.222877;  //user y-position here
+        var xDestination = 51.2079017; //end x-position here (open data)
+        var yDestination = 3.2221577; //end x-position here
+        var xCenter = (xStart + xDestination)/2;
+        var yCenter = (yStart + yDestination)/2;
+        var zoom = 18.75;
+        window.open("https://www.google.be/maps/dir/" + xStart + "," + yStart + "/" + xDestination + "," + yDestination + "/@" + xCenter + "," + yCenter + "," + zoom +"z");
+    }
 })();
