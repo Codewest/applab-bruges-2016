d26c016ded9d9a5b719b3e7ae1b74ab7fde823d0
John Laekh <laekh@hotmail.com>
2016-10-14 20:49:08 +0200

    Add development mode for authentication

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    if(isAuthenticated(req)) {
+    // DEVELOPMENT WITHOUT AUTH
+    res.render('index', { title: 'Applab' });
+
+    // PRODUCTION WITH AUTH
+    /*if(isAuthenticated(req)) {
         res.render('index', { title: 'Applab' });
     } else {
         res.render('login', { title: 'Applab' });
-    }
+    }*/
 });
 
 router.get('/login', function(req, res, next) {
