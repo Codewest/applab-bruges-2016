db5e4a842afc96289be8c8f6fb29638131777fe4
Michiel Leyman <michielleyman@gmail.com>
2016-10-14 17:47:08 +0200

    Moved routes to separate file

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,11 +1,3 @@
-var addEventListeners = function () {
-    $(".hamburger").on('click', toggleMenu);
-};
-
-var toggleMenu = function() {
-    $(".menu").slideToggle();
-};
-
 var xStart;
 var yStart;
 
@@ -25,16 +17,16 @@ function setPosition(position) {
 function showError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation.";
+            x.innerHTML = "User denied the request for Geolocation."
             break;
         case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable.";
+            x.innerHTML = "Location information is unavailable."
             break;
         case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out.";
+            x.innerHTML = "The request to get user location timed out."
             break;
         case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred.";
+            x.innerHTML = "An unknown error occurred."
             break;
     }
 }
@@ -46,23 +38,6 @@ var openRoute = function openRoute() {
     var yCenter = (yStart + yDestination)/2;
     var zoom = 18.75;
     window.open("https://www.google.be/maps/dir/" + xStart + "," + yStart + "/" + xDestination + "," + yDestination + "/@" + xCenter + "," + yCenter + "," + zoom +"z");
-};
+}
 
-(function() {
-    $(function () {
-        makeMap();
-        addEventListeners();
-        $("#route").on('click', openRoute);
-        setInterval(determinePosition, 5000);
-    });
-
-    var makeMap = function makeMap() {
-        mapboxgl.accessToken = "pk.eyJ1Ijoicm9vdHNhbSIsImEiOiJjaXU5a2V4aGcwMDFlMnRtazRibjZiZGN3In0.Q1K1wv1ugX49a4e1pCLMZw";
-        var map = new mapboxgl.Map({
-            container: "map",
-            style: "mapbox://styles/mapbox/light-v9",
-            center: [3.227180, 51.210358],
-            zoom: 13.7
-        });
-    };
-})();
+export { openRoute, determinePosition };
