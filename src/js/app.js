a657305a71af31f7309dea4b945acfe102fa8549
Sam <sam.demeulenaere@student.howest.be>
2016-10-14 14:24:39 +0200

    added map basics

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+$( document ).ready(function () {
+    makeMap();
+});
+
+
+var makeMap = function makeMap() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoicm9vdHNhbSIsImEiOiJjaXU5a2V4aGcwMDFlMnRtazRibjZiZGN3In0.Q1K1wv1ugX49a4e1pCLMZw';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v9',
+        center: [3.227180, 51.210358],
+        zoom: 13.7
+    });
+};
