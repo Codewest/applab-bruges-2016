82626d933af9ed45c5af5fd5798c49278b527ec5
Michiel Leyman <michielleyman@gmail.com>
2016-10-14 23:18:05 +0200

    Solved async json using promise chains

--- src/js/app.js
+++ src/js/app.js
@@ -20,8 +20,9 @@ import data from './data';
         addEventListeners();
         $("#route").on('click', openRoute);
         setInterval(determinePosition, 5000);
-        var points = data.benchPoints();
-        setMarkers(map, points);
+        data.getBenchPoints().then(function(points) {
+          setMarkers(map, points);
+        });
     });
 
     var makeMap = function makeMap() {
